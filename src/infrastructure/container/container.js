1cc55ceeb58035662d1da7c83d2056b807f044c3
Diego Perez <diegopm2000@gmail.com>
2020-04-30 19:30:09 +0200

    fixed exports of container.js
    better use of exports coupled to functions instead of using its in a module.exports as usual

--- src/infrastructure/container/container.js
+++ src/infrastructure/container/container.js
@@ -10,64 +10,45 @@ const moduleStore = {};
 // Config store
 let configStore = {};
 
-const set = (name, pathfile) => {
+exports.set = (name, pathfile) => {
   moduleStore[name] = require(pathfile);
 };
 
-const get = (name) => moduleStore[name];
+exports.get = (name) => moduleStore[name];
 
-const init = () => {
+exports.init = () => {
   // Logger
-  set('logger', '../log/logColorLogger');
+  exports.set('logger', '../log/logColorLogger');
   // Infrastructure Controllers (OpenApi Express Controllers)
-  set('healthcheckController', '../server/controller/healthcheckController');
-  set('configController', '../../infrastructure/server/controller/configController');
+  exports.set('healthcheckController', '../server/controller/healthcheckController');
+  exports.set('configController', '../../infrastructure/server/controller/configController');
   // Infrastructure Repositories
-  set('fileConfigRepository', '../repository/fileConfigRepository');
-  set('memConfigRepository', '../repository/memConfigRepository');
-  set('containerConfigRepository', '../repository/containerConfigRepository');
+  exports.set('fileConfigRepository', '../repository/fileConfigRepository');
+  exports.set('memConfigRepository', '../repository/memConfigRepository');
+  exports.set('containerConfigRepository', '../repository/containerConfigRepository');
   // Adapter Interface Components
-  set('configAdapterController', '../../adapter/controller/configAdapterController');
-  set('configJSONPresenter', '../../adapter/presenter/configJSONPresenter');
+  exports.set('configAdapterController', '../../adapter/controller/configAdapterController');
+  exports.set('configJSONPresenter', '../../adapter/presenter/configJSONPresenter');
 };
 
-const getLogger = () => get('logger');
+exports.getLogger = () => exports.get('logger');
 
-const getHealthcheckController = () => get('healthcheckController');
+exports.getHealthcheckController = () => exports.get('healthcheckController');
 
-const getConfigController = () => get('configController');
+exports.getConfigController = () => exports.get('configController');
 
-const getFileConfigRepository = () => get('fileConfigRepository');
+exports.getFileConfigRepository = () => exports.get('fileConfigRepository');
 
-const getMemConfigRepository = () => get('memConfigRepository');
+exports.getMemConfigRepository = () => exports.get('memConfigRepository');
 
-const getContainerConfigRepository = () => get('containerConfigRepository');
+exports.getContainerConfigRepository = () => exports.get('containerConfigRepository');
 
-const getConfigAdapterController = () => get('configAdapterController');
+exports.getConfigAdapterController = () => exports.get('configAdapterController');
 
-const getConfigJSONPresenter = () => get('configJSONPresenter');
+exports.getConfigJSONPresenter = () => exports.get('configJSONPresenter');
 
-const getConfig = () => configStore;
+exports.getConfig = () => configStore;
 
-const setConfig = (data) => {
+exports.setConfig = (data) => {
   configStore = data;
-};
-
-module.exports = {
-  init,
-  set,
-  get,
-  // Logger
-  getLogger,
-  // Infrastructure Controllers (OpenApi Express Controllers)
-  getHealthcheckController,
-  getConfigController,
-  // Infrastructure Repositories
-  getFileConfigRepository,
-  getMemConfigRepository,
-  getContainerConfigRepository,
-  getConfigAdapterController,
-  getConfigJSONPresenter,
-  getConfig,
-  setConfig,
 };
